bfe10e0de022d5284e5a1a9d08f6861c35418638
igniroits <w.hasithasenanayaka@gmail.com>
2021-01-29 09:44:55 +0530

    Update the tobe Change

--- tobe_exchanged.php
+++ tobe_exchanged.php
@@ -1,5 +1,5 @@
 <?php
-  error_reporting(0);
+  //error_reporting(0);
   include("db_config.php");
   $con = mysqli_connect(DB_HOSTNAME,DB_USERNAME,DB_PASSWORD,DB_NAME);
     if (!$con) {
@@ -16,9 +16,7 @@
 
 <?php
 
-// if(isset($_POST['btn3'])){
-
-  $query = mysqli_query($con,"SELECT B_id, cheque, cheque_no, cheque_date FROM bill WHERE cheque_status=1 ");
+  $query = mysqli_query($con,"SELECT B_id,cheque, cheque_no, cheque_date FROM bill WHERE cheque_status=1");
      
   $numRows = mysqli_num_rows($query);
 
@@ -30,10 +28,11 @@
       <td>                    <?php echo $row['cheque_no'] ?>   </td>
       <td class="text-right"> <?php echo $row['cheque'] ?>      </td>
       <td class="text-center">  
-      <a href="#" onclick="doneFUN('<?php echo $row['B_id']; ?>')" name="done">
+      <a href="#" onclick="doneFUN(<?php echo $row['B_id']; ?>)" name="done">
       <button class="btn btn-success btn-round">Done</button></a>
       </td>
     </tr>
+
   </tbody>
 <?php
       }
@@ -42,32 +41,31 @@
 </table>
 <?php
 
-$id=$_POST['id']; 
+  if(isset($_POST['id'])){
 
-$qry = mysqli_query($con,"SELECT cheque FROM bill WHERE B_id='$id' "); // select query
+      $id=$_POST['id']; 
+      $qry = mysqli_query($con,"SELECT cheque FROM bill WHERE B_id='$id' "); // select query
 
-  $data = mysqli_fetch_array($qry); // fetch data
-    $amt = $data['cheque'];
-    $ddate= cur_date('Y-m-d');
+      $data = mysqli_fetch_array($qry); // fetch data
+      $amt = $data['cheque'];
+      $ddate= date("Y-m-d");;
 
-if(isset($_POST['done'])){
+      $add = mysqli_query($con,"INSERT INTO debt (ddate,amt)VALUES('$ddate',$amt)");
+      $state = mysqli_query($con,"UPDATE bill SET cheque_status=0 WHERE B_id =  '$id'");
+  }
 
-  $add = mysqli_query($con,"INSERT INTO debt (ddate,amt)VALUES('$ddate',$amt)");
-  $state = mysqli_query($con,"UPDATE bill SET cheque_status=0 WHERE B_id =  '$id'");
-
-}
 ?>
 
 
-  <?php
+<?php
   mysqli_close($con);
-
-  // }
-
  ?>
+
 <script>
+
 function doneFUN(id){
-alert(id)
+
+  alert(id)
   $.ajax({
     url:"tobe_exchanged.php",
     method:"POST",
@@ -75,7 +73,7 @@ alert(id)
     success:function(data){
       swal({
         title: "Good job !",
-        text: data,
+        text: "Successfully Updated the Status",
         icon: "success",
         button: "Ok !",
         });
